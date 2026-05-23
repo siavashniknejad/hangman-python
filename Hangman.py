491ab4fe540f3dae4d7613df07cd29fb1e3940d3
Siavash <siavash@Siavashs-MacBook-Air.local>
2026-05-23 11:25:17 +0200

    clean project

--- Hangman.py
+++ Hangman.py
@@ -1,16 +1,20 @@
-word_list = ["python", "hangman", "programming", "challenge", "developer"]
 import random
+word_list = ["python", "hangman", "programming", "challenge", "developer"]
 word = random.choice(word_list)
-print  (word)
-# character = list(word)
+print(word)
 guessed_letters = []
-while True : 
+while True:
     guess_letter = input("Guess a letter:")
-    guessed_letters.append(guess_letter)   
+    guessed_letters.append(guess_letter)
     count_char = len(word)
-    for i in range(count_char) :
-        if word[i] in guessed_letters :
-            print (word[i], end=" ")
-        else :
-            print("-", end=" ") 
+    all_guessed = True
+    for i in range(count_char):
+        if word[i] in guessed_letters:
+            print(word[i], end=" ")
+        else:
+            print("-", end=" ")
+            all_guessed = False
     print()
+    if all_guessed:
+        print("You win!")
+        break
